f33c8fc45691d0977527b9a4522d30b6bc0cb51b
Sangramsin9 <engg.sangramsing@gmail.com>
2020-10-05 10:44:07 +0530

    Refactored API to find possible moves for a King

--- src/main/java/com/oops/design/chess/pieces/King.java
+++ src/main/java/com/oops/design/chess/pieces/King.java
@@ -18,47 +18,22 @@ public class King extends Piece {
         int row = getSquare().getRow();
         int col = getSquare().getColumn();
 
-        //down
-        Square square = getSquare().getBoardSquare(row+1, col);
-        if (square != null && square.getPiece() == null) {
-            possibleMoves.add(square);
-        }
+        int arr [][] = {
+                {row+1, col},
+                {row-1, col},
+                {row, col+1},
+                {row, col-1},
+                {row+1, col+1},
+                {row+1, col-1},
+                {row-1, col-1},
+                {row-1, col+1}
+        };
 
-        // up
-        square = getSquare().getBoardSquare(row-1, col);
-        if (square != null && square.getPiece() == null) {
-            possibleMoves.add(square);
-        }
-        // left
-        square = getSquare().getBoardSquare(row, col+1);
-        if (square != null && square.getPiece() == null) {
-            possibleMoves.add(square);
-        }
-        // left
-        square = getSquare().getBoardSquare(row, col-1);
-        if (square != null && square.getPiece() == null) {
-            possibleMoves.add(square);
-        }
-
-        // down left
-        square = getSquare().getBoardSquare(row+1, col+1);
-        if (square != null && square.getPiece() == null) {
-            possibleMoves.add(square);
-        }
-        // down right
-        square = getSquare().getBoardSquare(row+1, col-1);
-        if (square != null && square.getPiece() == null) {
-            possibleMoves.add(square);
-        }
-        // up left
-        square = getSquare().getBoardSquare(row-1, col-1);
-        if (square != null && square.getPiece() == null) {
-            possibleMoves.add(square);
-        }
-        // up right
-        square = getSquare().getBoardSquare(row-1, col+1);
-        if (square != null && square.getPiece() == null) {
-            possibleMoves.add(square);
+        for (int a[] : arr) {
+            Square square = getSquare().getBoardSquare(a[0],a[1]);
+            if (square != null && square.getPiece() == null) {
+                possibleMoves.add(square);
+            }
         }
 
         return possibleMoves;
